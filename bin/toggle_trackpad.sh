8449e7545e234feadd96836ab1eebd877e371d40
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2022-06-18 15:58:16 +0200

    chore: fix bash pipes formatting

--- bin/toggle_trackpad.sh
+++ bin/toggle_trackpad.sh
@@ -7,7 +7,7 @@ ID=$(
     | grep -Eo '[0-9]{1,2}'
 )
 declare -i STATE
-STATE=$(xinput list-props "${ID}" | grep 'Device Enabled'|awk '{print $4}')
+STATE=$(xinput list-props "${ID}" | grep 'Device Enabled' | awk '{print $4}')
 if [ "${STATE}" -eq 1 ]
 then
     xinput disable "${ID}"
